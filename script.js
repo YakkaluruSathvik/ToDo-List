9fad08f07ae6f799e887334da3c4fad2ff208cad
ysathvik <yakkalurusathvik1@gmail.com>
2023-10-09 20:05:40 +0530

    Updated script.js

--- script.js
+++ script.js
@@ -17,6 +17,16 @@ function addTask(){
     saveData();
 }
 
+inputBox.addEventListener("keypress", function(event) {
+    // If the user presses the "Enter" key on the keyboard
+    if (event.key === "Enter") {
+      // Cancel the default action, if needed
+      event.preventDefault();
+      // Trigger the button element with a click
+      document.getElementById("btn").click();
+    }
+  });
+
 list.addEventListener("click",function(e){
     if(e.target.tagName === "LI"){
         e.target.classList.toggle("checked");
